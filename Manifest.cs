a9b349f1366c54c97ad6e11b482ea6ff8bab1f9b
Gabriel Alberto Castillo Prada <g.castillo@teo1wkl145.go.johnsoncontrols.com>
2019-08-20 15:46:43 -0500

    Initial commit

---
+++ Manifest.cs
@@ -0,0 +1,9 @@
+using OrchardCore.DisplayManagement.Manifest;
+
+[assembly: Theme(
+    Name = "TheCreativeTheme",
+    Author = "The Create Team",
+    Website = "https://orchardproject.net",
+    Version = "0.0.1",
+    Description = "TheCreativeTheme"
+)]
